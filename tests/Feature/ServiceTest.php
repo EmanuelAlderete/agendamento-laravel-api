1a5ec0885e38715657b9d15f0c486c429b89336f
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-09 16:31:37 -0300

    test: adiciona teste para criação de Service

---
+++ tests/Feature/ServiceTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ServiceTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_authenticated_user_can_create_a_service(): void
+    {
+        $professional_user = User::factory()->create(['type' => 'professional']);
+
+        $payload = [
+            'name' => 'Test Service',
+            'price' => 100.00,
+            'duration_minutes' => 60,
+        ];
+
+        $this->actingAs($professional_user)
+            ->postJson('/services', $payload)
+            ->assertCreated()
+            ->assertJson([
+                'data' => [
+                    'name' => $payload['name'],
+                    'price' => $payload['price'],
+                    'duration_minutes' => $payload['duration_minutes'],
+                    'professional_id' => $professional_user->id,
+                ]
+            ]);
+
+        $this->assertDatabaseHas('services', [
+            'professional_id' => $professional_user->id,
+            'name' => $payload['name'],
+            'price' => $payload['price'],
+            'duration_minutes' => $payload['duration_minutes'],
+        ]);
+
+
+    }
+}
