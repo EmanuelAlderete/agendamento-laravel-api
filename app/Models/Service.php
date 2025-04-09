d7c4b8564c2a07c6713d6b4db2c5dd15539db96c
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-09 11:23:21 -0300

    feat: cria tabela services e adiciona relacionamentos em User e Service

---
+++ app/Models/Service.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Service extends Model
+{
+    protected $fillable = [
+        'professional_id',
+        'name',
+        'price',
+        'duration_minutes',
+    ];
+
+    public function professional(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'professional_id');
+    }
+}
